eb028915d6f25af4c04099f861ddc7eea7ed991f
Ant <antoniolopez18022002@gmail.com>
2025-05-03 23:54:24 +0200

    connections between modules parsed

--- BaseModule.py
+++ BaseModule.py
@@ -1,7 +1,7 @@
 from random import randint
 
 class BaseModule:
-  id: int
+  id: int # Given from frontend
   name: str
   # Position, will vary in the future (after solving for positioning)
   posX: int
@@ -10,44 +10,14 @@ class BaseModule:
   sizeX: int
   sizeY: int
   price: int
-  # Connection with other modules
-  connections: dict 
+
+  connectedIn: list[str] # List of ids of connected modules
+  connectedOut: list[str] # List of ids of connected modules
 
 
   def __init__(self, name):
-    self.id = randint(0, 1000000)  # Random ID for the module
     self.name = name
     self.posX = 0
     self.posY = 0
-    self.connections = {
-      'input': [],
-      'output': []
-    }
-
-  
-  def setInput(self, other_object_id):
-    if other_object_id in self.connections['input']:
-      print(f"Input connection with ID {other_object_id} already exists.")
-    else:
-      self.connections['input'].append(other_object_id)
-
-
-  def setOutput(self, other_object_id):
-    if other_object_id in self.connections['output']:
-      print(f"Output connection with ID {other_object_id} already exists.")
-    else:
-      self.connections['output'].append(other_object_id)
-
-
-  def removeInput(self, other_object_id):
-    if other_object_id in self.connections['input']:
-      self.connections['input'].remove(other_object_id)
-    else:
-      print(f"Input connection with ID {other_object_id} not found for removal.")
-
-
-  def removeOutput(self, other_object_id):
-    if other_object_id in self.connections['output']:
-      self.connections['output'].remove(other_object_id)
-    else:
-      print(f"output connection with ID {other_object_id} not found for removal.")
+    self.connectedIn = []
+    self.connectedOut = []
