2536f9a4519d61fcca8ec4422f12b8728f14c2dd
pllinasv <pol.llinas06@gmail.com>
2025-05-03 09:36:09 +0200

    added route

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
-from  Modules import Data_Rack
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
-data_rack = Data_Rack("rack1",2)
+app = FastAPI()
 
-print(data_rack)
+@app.get("/")
+def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
+@app.get("/test")
+def test():
+    return {
+        "source": [
+            {
+                "name": "Solar Panel",
+                "icon": "Sun",
+                "specs": ["Output: 20W", "Type: Renewable"],
+                "cost": 2000,
+            },
+        ],
+        "sink": [
+            {
+                "name": "Server",
+                "icon": "Server",
+                "specs": ["Consumption: 10W", "Critical Load"],
+                "cost": 1500,
+            },
+            {
+                "name": "Cooler",
+                "icon": "Snowflake",
+                "specs": ["Consumption: 5W", "Thermal Control"],
+                "cost": 1000,
+            },
+        ],
+    }
+
