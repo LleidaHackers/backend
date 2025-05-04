095d6e7f380f45b610d1d330e3da79c0e1bb302f
Pol Escolà Curcó <polescola3000@gmail.com>
2025-05-04 02:58:54 +0200

    fix conflict

--- DataCenters/Server_Square.py
+++ DataCenters/Server_Square.py
@@ -1,7 +1,7 @@
 from DataCenterBase import DataCenterBase
 
 class ServerSquare(DataCenterBase):
-  def __init__(self, ):
+  def __init__(self):
     super().__init__(gridConnection=3, waterConnection=1, spaceX=1000, spaceY=500)
     self.externalNetwork: int = 0    # maximize, "objective"
     self.dataStorage: int = 100      # min
