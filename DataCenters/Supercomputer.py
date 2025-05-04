c1623d9f58177da58299c44d161daa670aa48dca
Ant <antoniolopez18022002@gmail.com>
2025-05-04 03:25:27 +0200

    fixing issues with the api

--- DataCenters/Supercomputer.py
+++ DataCenters/Supercomputer.py
@@ -2,6 +2,6 @@ from DataCenterBase import DataCenterBase
 
 class Supercomputer(DataCenterBase):
   def __init__(self, gridConnection, waterConnection, spaceX, spaceY):  
-    super().__init__(self, gridConnection, waterConnection, spaceX, spaceY)
+    super().__init__(gridConnection, waterConnection, spaceX, spaceY)
     self.usablePower : int = 0     # minimize
     self.processing: int = 0       # maximize
