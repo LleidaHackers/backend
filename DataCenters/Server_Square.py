fdb40d880852a2094616599cf913e7969c99ec63
Ant <antoniolopez18022002@gmail.com>
2025-05-04 03:15:32 +0200

    little fix

--- DataCenters/Server_Square.py
+++ DataCenters/Server_Square.py
@@ -2,7 +2,7 @@ from DataCenterBase import DataCenterBase
 
 class ServerSquare(DataCenterBase):
   def __init__(self, gridConnection, waterConnection, spaceX, spaceY):
-    super().__init__(gridConnection, waterConnection, spaceX, spaceY)
+    super().__init__(self, gridConnection, waterConnection, spaceX, spaceY)
     self.externalNetwork: int = 0    # maximize, "objective"
     self.dataStorage: int = 100      # min
     self.processing: int = 1000      # min 
